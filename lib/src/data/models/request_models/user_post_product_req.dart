306b0434c56fc62a6a8931065ca000d90a903582
Mohammed Ragheb <eellrraawwii@gmail.com>
2024-02-20 16:18:16 +0300

    Fix to json in UserPostProductReq

--- lib/src/data/models/request_models/user_post_product_req.dart
+++ lib/src/data/models/request_models/user_post_product_req.dart
@@ -23,7 +23,7 @@ class UserPostProductReq {
     }
 
     if (product.images != null) {
-      data['images'] = product.images!.map((e) => e.id).toList();
+      data['images'] = product.images!.map((e) => e.url).toList();
     }
 
     if (product.thumbnail != null) {
